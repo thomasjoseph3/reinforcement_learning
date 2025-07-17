5f1a7c98b7853dcbc1a61a443eba7efcfd3b1627
thomasjoseph3 <thomas.joseph.tblr@gmail.com>
2025-07-17 17:18:20 +0530

    qlearning implimented

---
+++ tutorial/qlearning.py
@@ -0,0 +1,42 @@
+import numpy as np
+import gymnasium as gym
+import pickle as pkl
+
+cliff_env = gym.make("CliffWalking-v0", render_mode="ansi")
+q_table = np.zeros((48, 4))
+
+# Q-learning parameters
+EPSILON = 0.1  # Exploration rate
+ALPHA = 0.1    # Learning rate
+GAMMA = 0.9    # Discount factor
+NUM_EPISODES = 500
+
+
+def policy(state, explore=0.0):
+    action = int(np.argmax(q_table[state]))
+    if np.random.random() <= explore:
+        action = int(np.random.randint(low=0, high=4, size=1))
+    return action
+
+for episode in range(NUM_EPISODES):
+    done = False
+    (state, info) = cliff_env.reset()
+    total_reward = 0
+    episode_length = 0
+    while not done:
+        action = policy(state, EPSILON)
+        next_state, reward, terminated, truncated, info = cliff_env.step(action)
+        # Q-learning update: use max Q for next state (off-policy)
+        best_next_action = np.argmax(q_table[next_state])
+        q_table[state][action] += ALPHA * (
+            reward + GAMMA * q_table[next_state][best_next_action] - q_table[state][action]
+        )
+        state = next_state
+        total_reward += reward
+        episode_length += 1
+        done = terminated or truncated
+    print(total_reward, "total reward", "episode", episode_length, "episode", episode)
+
+cliff_env.close()
+pkl.dump(q_table, open("qlearning_qtable.pkl", "wb"))
+print("Q-learning training complete")
--- tutorial/sarsa.py
+++ tutorial/sarsa.py
@@ -1,5 +1,5 @@
 import numpy as np
-import gymnasium as gym
+import gymnasium as gym # type: ignore
 import pickle as pkl
 cliff_env = gym.make("CliffWalking-v0", render_mode="ansi")
 q_table = np.zeros(shape=(48, 4))
@@ -13,8 +13,8 @@ def policy(state, explore: float = 0.0):
 
 
 # parameters
-EPSILON = 0.1
-ALPHA = 0.1
+EPSILON = 0.09
+ALPHA = 0.07
 GAMMA = 0.9
 NUM_EPISODES = 500
 
@@ -26,10 +26,8 @@ for episodes in range(NUM_EPISODES):
     episodes_length = 0
     action = policy(state, EPSILON)
     while not done:
-
         next_state, reward, terminated, truncated, info = cliff_env.step(action)
         next_action = policy(next_state, EPSILON)
-
         q_table[state][action] += ALPHA * (
             reward + GAMMA * q_table[next_state][next_action] - q_table[state][action]
         )
@@ -38,18 +36,9 @@ for episodes in range(NUM_EPISODES):
         total_reward += reward
         episodes_length += 1
         done = terminated or truncated
-    print(total_reward, "total reward", "episode", episodes_length,"episode",episodes)
+    print(total_reward, "total reward", "episode", episodes_length, "episode", episodes)
         
 cliff_env.close()
 pkl.dump(q_table,open("sarsa_qtable.pkl","wb"))
 print("training complete")
 
-# done = False
-# (state, info) = cliff_env.reset()
-
-# while not done:
-#     # print(cliff_env.render())
-#     action = int(np.random.randint(low=0, high=4, size=1))  # actions: 0,1,2,3
-#     next_state, reward, terminated, truncated, info = cliff_env.step(action)
-#     done = terminated or truncated
-# cliff_env.close()
